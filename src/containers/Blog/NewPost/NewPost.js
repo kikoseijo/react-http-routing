20678833736de85fe59027c02fd5454284c17165
Kiko Seijo <kiko@sunnyface.com>
2017-10-29 20:15:48 +0100

    Lesson 200

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -9,10 +9,12 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Max',
+        submitted: false
     }
 
     componentDidMount () {
-        console.log(this.props);
+        // If unauth => this.props.history.replace('/posts');
+        console.log( this.props );
     }
 
     postDataHandler = () => {
@@ -21,24 +23,29 @@ class NewPost extends Component {
             body: this.state.content,
             author: this.state.author
         };
-        axios.post('/posts', data)
-            .then(response => {
-                console.log(response);
-                this.props.history.push('/posts');
-            });
+        axios.post( '/posts', data )
+            .then( response => {
+                console.log( response );
+                this.props.history.replace('/posts');
+                // this.setState( { submitted: true } );
+            } );
     }
 
     render () {
-
+        let redirect = null;
+        if (this.state.submitted) {
+            redirect = <Redirect to="/posts" />;
+        }
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input type="text" value={this.state.title} onChange={( event ) => this.setState( { title: event.target.value } )} />
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.content} onChange={( event ) => this.setState( { content: event.target.value } )} />
                 <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                <select value={this.state.author} onChange={( event ) => this.setState( { author: event.target.value } )}>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
