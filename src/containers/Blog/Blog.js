0841199dcdf2f0d05faaa0e7c6d9258bdb50ea64
Kiko Seijo <kiko@sunnyface.com>
2017-10-29 17:06:33 +0100

    Lesson 176

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,45 +1,17 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+
+
 import './Blog.css';
+import Post from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        users: [],
-        selectedPostId: null,
-    }
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0,6);
-                const updatedPosts = posts.map( post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                });
-                this.setState({posts:updatedPosts});
-            });
-        axios.get('/users')
-            .then(response => {
-                this.setState({users:response.data})
-            });
-    }
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
 
-    }
+
+
     render () {
-        const posts = this.state.posts.map( post => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                click={() => this.postSelectedHandler(post.id)}/>
-        })
+
         return (
             <div className="Blog">
                 <header>
@@ -50,10 +22,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    { posts }
-                </section>
-                
+                <Posts />
+                <FullPost id={this.state.selectedPostId}/>
+                <NewPost />
             </div>
         );
     }
